91324c49d102ba7c49046e7c8879478ce635194c
Dario Puddinu <dario.puddinu@abinsula.com>
2024-07-22 09:50:34 +0200

    chore: add basic controllers and routes

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import bcrypt from 'bcryptjs';
+import prisma from '../src/lib/prisma';
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      password: bcrypt.hashSync('password', 8),
+      appointments: {
+        create: {
+          description: 'Bench 220 pounds',
+          creationDate: new Date(),
+          updateDate: new Date(),
+          dueDate: undefined,
+          category: {
+            create: {
+              name: 'Gym'
+            }
+          }
+        }
+      }
+    }
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      password: bcrypt.hashSync('password', 8),
+      appointments: {
+        create: {
+          description: 'Go to the dentist',
+          creationDate: new Date(),
+          updateDate: new Date(),
+          dueDate: undefined,
+           category: {
+            create: {
+              name: 'Health'
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcryptjs';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt from '../utils/jwt';
+import prisma from '@/lib/prisma';
+
+class AuthController {
+  async authenticate(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next({
+        status: StatusCodes.BAD_REQUEST,
+        message: 'Some required fields are missing'
+      });
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return next({
+        status: StatusCodes.NOT_FOUND,
+        message: 'User not found'
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return next({
+        status: StatusCodes.UNAUTHORIZED,
+        message: 'Invalid password'
+      });
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email });
+
+    res.status(StatusCodes.OK).json({ token });
+  }
+}
+
+export default new AuthController();
