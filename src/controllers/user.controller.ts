3bfaf8a41aaf679ba17a8bc5e2cb5ac874f96597
Dario Puddinu <dario.puddinu@abinsula.com>
2024-07-30 10:55:33 +0200

    fix: get user by id json

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,15 +4,13 @@ import prisma from '#controllers/lib/prisma';
 
 class UserController {
   async getById(req: Request, res: Response, next: NextFunction) {
-    console.log(res.locals.payload); // just to see the payload
-
     const user = await prisma.user.findUnique({
       where: { id: Number(req.params.id) }
     });
 
     if (!user) return next({ status: StatusCodes.NOT_FOUND, message: 'User not found' });
 
-    res.status(StatusCodes.OK).json({ name: user.name, email: user.email });
+    res.status(StatusCodes.OK).json({ id: user.id, email: user.email });
   }
 }
 
